3f7ca0dddb3eaa33091e6d7bf888e6bb8328b631
Fang Yi Liu <fangyiliu@gmail.com>
2020-08-11 22:55:50 -0700

    Scaffold home page elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Header from './elements/Header';
+import Home from './Home';
 
 const App = () => (
   <div>
     <Header />
-    Start Here!
+    <Home />
   </div>
 );
 
