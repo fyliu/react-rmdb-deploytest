17597a0acc5f12187a502d087ccc5b1bf569bf40
Fang Yi Liu <fangyiliu@gmail.com>
2020-08-15 00:09:25 -0700

    Created styled component for header

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Header = () => <div>Header</div>;
+import RMDBLogo from '../images/reactMovie_logo.png';
+import TMDBLogo from '../images/tmdb_logo.svg';
+
+const StyledHeader = styled.div`
+  background: #1c1c1c;
+  padding: 0 20px;
+  box-sizing: border-box;
+
+  .header-content {
+    max-width: 1280px;
+    min-height: 120px;
+    padding: 20px 0;
+    margin: 0 auto;
+    box-sizing: border-box;
+
+    @media screen and (max-width: 500px) {
+      in-height: 0px;
+    }
+  }
+`;
+
+const Header = () => (
+  <StyledHeader>
+    <div className='header-content'>
+      <img src={RMDBLogo} alt='rmdb-logo' />
+      <img src={TMDBLogo} alt='tmdb-logo' />
+    </div>
+  </StyledHeader>
+);
 
 export default Header;
