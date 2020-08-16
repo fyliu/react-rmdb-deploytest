eb2927a73c9fb15ec586b2d48babeb4c494fe340
Fang Yi Liu <fangyiliu@gmail.com>
2020-08-15 23:30:57 -0700

    Use the styled components in separate style file

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,48 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import RMDBLogo from '../images/reactMovie_logo.png';
 import TMDBLogo from '../images/tmdb_logo.svg';
 
-const StyledHeader = styled.div`
-  background: #1c1c1c;
-  padding: 0 20px;
-  box-sizing: border-box;
-
-  .header-content {
-    max-width: 1280px;
-    min-height: 120px;
-    padding: 20px 0;
-    margin: 0 auto;
-    box-sizing: border-box;
-
-    @media screen and (max-width: 500px) {
-      in-height: 0px;
-    }
-  }
-`;
-
-const StyledRMDBLogo = styled.img`
-  width: 250px;
-  margin-top: 20px;
-
-  @media screen and (max-width: 500px) {
-    width: 150px;
-    margin-top: 5px;
-  }
-`;
-
-const StyledTMDBLogo = styled.img`
-  width: 122px;
-  margin-top: 25px;
-  float: right;
-
-  @media screen and (max-width: 500px) {
-    display: inline-block;
-    width: 80px;
-    margintop: 0px;
-  }
-`;
+import {
+  StyledHeader,
+  StyledRMDBLogo,
+  StyledTMDBLogo,
+} from '../styles/StyledHeader';
 
 const Header = () => (
   <StyledHeader>
