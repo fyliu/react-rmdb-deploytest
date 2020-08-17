c40be2c5d866813fc60c9c652a2103721ce35a28
Fang Yi Liu <fangyiliu@gmail.com>
2020-08-16 23:26:35 -0700

    Use imported GlobalStyle component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
 
 import Header from './elements/Header';
 import Home from './Home';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
+import { GlobalStyle } from './styles/GlobalStyle';
 
 const App = () => (
   <>
