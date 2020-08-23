4fb9522d4912d98a28cb5c37429ea60afdfd19e9
Fang Yi Liu <fangyiliu@gmail.com>
2020-08-22 23:35:45 -0700

    Set delay timer for search bar

--- src/components/elements/SearchBar.js
+++ src/components/elements/SearchBar.js
@@ -11,9 +11,13 @@ const SearchBar = ({ callback}) => {
   const timeOut = useRef(null);
 
   const doSearch = (event) => {
-    console.log(event.target.value);
     const { value } = event.target;
+    clearTimeout(timeOut.current);
     setState(value);
+
+    timeOut.current = setTimeout(() => {
+      callback(value);
+    }, 500);
   };
 
   return (
