4925e41a05397bd8d8b26f8ffa86c64830a3434a
Fang Yi Liu <fangyiliu@gmail.com>
2020-08-23 21:06:14 -0700

    Add movie info details

--- src/components/elements/MovieInfo.js
+++ src/components/elements/MovieInfo.js
@@ -20,6 +20,24 @@ const MovieInfo = ({ movie }) => (
           clickable={false}
         />
       </div>
+      <div className='movieinfo-text'>
+        <h1>{movie.title}</h1>
+        <h3>PLOT</h3>
+        <p>{movie.overview}</p>
+
+        <div className='rating-director'>
+          <div>
+            <h3>IMDB RATING</h3>
+            <div className='score'>{movie.vote_average}</div>
+          </div>
+          <div classname='director'>
+            <h3>DIRECTOR{movie.directors.length > 1 ? 'S' : ''}</h3>
+            {movie.directors.map((element) => (
+              <p key={element.credit_id}>{element.name}</p>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   </StyledMovieInfo>
 );
