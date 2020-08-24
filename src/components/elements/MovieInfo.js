66959df5cdb627502d394c470e52da0f7bede4a7
Fang Yi Liu <fangyiliu@gmail.com>
2020-08-23 20:54:53 -0700

    Build MovieInfo component

--- src/components/elements/MovieInfo.js
+++ src/components/elements/MovieInfo.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
-const MovieInfo = () => <div>MovieInfo</div>;
+import NoImage from '../images/no_image.jpg';
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
+
+import MovieThumb from './MovieThumb';
+
+import { StyledMovieInfo } from '../styles/StyledMovieInfo';
+
+const MovieInfo = ({ movie }) => (
+  <StyledMovieInfo backdrop={movie.backdrop_path}>
+    <div className='movieinfo-content'>
+      <div className='movieinfo-thumb'>
+        <MovieThumb
+          image={
+            movie.poster_path
+              ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+              : NoImage
+          }
+          clickable={false}
+        />
+      </div>
+    </div>
+  </StyledMovieInfo>
+);
 
 export default MovieInfo;
